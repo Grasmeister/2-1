9f2d18550785553cb3e0ce7cf2fcc605ff1c954c
AntonT <Grasmeister@list.ru>
2022-07-01 15:19:33 +0300

    2.1.1 version 1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,20 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        Cat bean3 =
+                (Cat) applicationContext.getBean("Cat");
+        System.out.println(bean3.getName());
+
+        Cat bean4 =
+                (Cat) applicationContext.getBean("Cat");
+        System.out.println(bean4.getName() + "2");
+        System.out.println(bean.equals(bean1));
+        System.out.println(bean3.equals(bean4));
+        System.out.println(bean1 == bean);
+        System.out.println(bean3 == bean4);
     }
 }
